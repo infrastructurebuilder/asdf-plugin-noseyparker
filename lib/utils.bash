65e33c0f8e1448ecb6e491394c49b230755bc496
Mykel Alvis <mykel.alvis@gmail.com>
2025-09-25 09:48:51 -0500

    Working

--- lib/utils.bash
+++ lib/utils.bash
@@ -81,17 +81,16 @@ install_version() {
 		fail "asdf-$TOOL_NAME supports release installs only"
 	fi
 
-	echo "* Installing $TOOL_NAME version $version... from $ASDF_DOWNLOAD_PATH to $install_path"
+	# echo "* Installing $TOOL_NAME version $version... from $ASDF_DOWNLOAD_PATH to $install_path"
 	(
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert noseyparker executable exists.
+		# Assert noseyparker executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 
-		#test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
-		test -x "$install_path/$tool_cmd"
+		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
 		echo "$TOOL_NAME $version installation was successful!"
 	) || (
